efeb29036e921c5678a94e3d1dbc97508a6f1ef6
kwazzar <fugitiwe@gmail.com>
2025-02-22 19:37:50 +0200

    Initial Commit

---
+++ PetTrasnslator/PetTrasnslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTrasnslatorApp.swift
+//  PetTrasnslator
+//
+//  Created by Quasar on 22.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetTrasnslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
